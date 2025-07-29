3e864c137636815e6e83714e6ee4b62070211e41
cytus <hendri.stti@gmail.com>
2025-07-29 11:17:10 +0700

    add a middlewares and rework auth

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -3,6 +3,8 @@ import * as Yup from "yup";
 
 import UserModel from "../models/user.model";
 import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middlewares/auth.middlewares";
 
 type TRegister = {
     fullName: string;
@@ -88,9 +90,14 @@ export default {
                 });
             }
 
+            const token = generateToken({
+                id: userByIdentifier._id,
+                role: userByIdentifier.role,
+            });
+
             res.status(200).json({
                 message: "Login Success",
-                data: userByIdentifier,
+                data: token,
             });
         } catch (error) {
             const err = error as unknown as Error;
@@ -99,5 +106,24 @@ export default {
                 data: null,
             });
         }
-    }
-}
+    },
+
+    async me(req: IReqUser, res: Response) {
+        try {
+            const user = req.user;
+            const result = await UserModel.findById(user?.id);
+
+            res.status(200).json({
+                message: "Success Get User Profile",
+                data: result,
+            })
+
+        } catch (error) {
+            const err = error as unknown as Error;
+            res.status(400).json({
+                message: err.message,
+                data: null,
+            });
+        }
+    },
+};
