f4de6cb375e897f645e35ab8253e0d9c80f1210b
cytus <hendri.stti@gmail.com>
2025-07-29 12:27:34 +0700

    add / endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,24 @@ import db from "./utils/database";
 async function init() {
     try{
         const result = await db();
+
         console.log("Database Status: ", result)
+
         const app = express();
+
         app.use(bodyParser.json());
+
         const PORT = 3000;  
+
+        app.get('/', (req, res) => {
+            res.status(200).json({
+                message: "Server is Running",
+                data: null,
+            });
+        });
+
         app.use('/api', router);
+        
         app.listen(PORT, ()=>{
             console.log(`server is running on http://localhost:${PORT}`);
         });
